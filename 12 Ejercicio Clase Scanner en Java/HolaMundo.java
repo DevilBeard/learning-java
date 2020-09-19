4b06618481989c7fb9a16ec67b4ab864e12c7c48
DevilBeard <calejoalar@gmail.com>
2020-09-19 16:43:27 -0500

    Clase 12

--- 12 Ejercicio Clase Scanner en Java/HolaMundo.java
+++ 12 Ejercicio Clase Scanner en Java/HolaMundo.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo de mmrd!!!!");
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Proporcione un titulo:");
+        var titulo = scn.nextLine();
+
+        System.out.println("Escribe un autor:");
+        var autor = scn.nextLine();
+        System.out.println("\"" + titulo + "\"" + " fue escrito por " + autor);
     }
 }
